289675d91bfc1a488552582e83ca724630cdb97f
lcy <lcy@IFCHIC-MacBook-Pro.local>
2024-04-26 12:26:12 +0800

    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/lcy-aliens' : ''
+  basePath: isProd ? '/lcy-aliens' : '',
+  assetPrefix: isProd ? '/lcy-aliens/' : ''
 };
 
 export default nextConfig;
