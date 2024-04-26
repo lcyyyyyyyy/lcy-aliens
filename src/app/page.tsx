ad36597749c2cec440de204ea5ffc17148227712
lcy <lcy@IFCHIC-MacBook-Pro.local>
2024-04-26 14:45:22 +0800

    log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,12 @@ import { Client } from '@notionhq/client'
 import { prefix } from '../utils/prefix'
 
 export default async function Home() {
-  const notion = new Client({ auth: process.env.NEXT_PUBLIC_NOTION_TOKEN })
-  const pages = await notion.databases.query({ database_id: process.env.NEXT_PUBLIC_NOTION_DATABASE_ID! })
+  // const notion = new Client({ auth: process.env.NEXT_PUBLIC_NOTION_TOKEN })
+  // const pages = await notion.databases.query({ database_id: process.env.NEXT_PUBLIC_NOTION_DATABASE_ID! })
 
   // console.log(pages.results[0].properties.images.files)
+  console.log(process.env.NEXT_PUBLIC_NOTION_TOKEN);
+  
 
 
   return (
