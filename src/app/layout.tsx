4316b0d39bc394b752aea9b39623abd1743550d7
lcy <lcy@IFCHIC-MacBook-Pro.local>
2024-07-31 14:16:38 +0800

    Update .ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from 'next'
 import { Suspense } from 'react'
 import localFont from 'next/font/local'
+import Favicon from './icon.ico'
 import '../styles/default.scss'
 
 const ChenYuluoyan = localFont({ src: '../../public/fonts/ChenYuluoyan-Thin-Monospaced.ttf' })
 
 export const metadata: Metadata = {
+  icons: [{ rel: 'icon', url: Favicon.src }],
   title: 'lcy Store',
   keywords: 'lcy Store,三眼怪,玩具總動員,alien,toy story',
   description: '購買最可愛的三眼怪商品！'
